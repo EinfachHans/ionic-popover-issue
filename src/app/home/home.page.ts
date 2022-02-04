6ea3f38d1f2dec49a2cd228272d334f7bff22ffe
EinfachHans <hanskrywaa@web.de>
2022-02-04 13:32:17 +0100

    init

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { PopoverController } from '@ionic/angular';
+import { PopoverComponent } from '../popover/popover.component';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,15 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private popoverCtrl: PopoverController) {}
 
+  present(event: MouseEvent) {
+    this.popoverCtrl.create({
+      component: PopoverComponent,
+      event,
+      cssClass: 'popover-fc'
+    }).then((popover) => {
+      popover.present();
+    });
+  }
 }
